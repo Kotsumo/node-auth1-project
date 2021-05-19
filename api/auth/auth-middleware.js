9b6c5c1ff46cc3c93a176b544677b60a76f64246
David <davidellis1020@gmail.com>
2021-05-18 17:20:56 -0700

    setup framework

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -6,8 +6,8 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  next()
 }
 
 /*
@@ -18,7 +18,7 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
+function checkUsernameFree(req, res, next) {
 
 }
 
@@ -30,7 +30,7 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
+function checkUsernameExists(req, res, next) {
 
 }
 
@@ -42,8 +42,14 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
+function checkPasswordLength(req, res, next) {
 
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+}
